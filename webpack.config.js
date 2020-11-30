47221905f941903886d541dfcd7b3bc88ba9003d
isoppp <prog@d2deck.com>
2020-11-30 22:03:01 +0900

    format

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
-const path = require('path');
-const slsw = require('serverless-webpack');
-const nodeExternals = require('webpack-node-externals');
-const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+const path = require('path')
+const slsw = require('serverless-webpack')
+const nodeExternals = require('webpack-node-externals')
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin')
 
 module.exports = {
   context: __dirname,
@@ -51,4 +51,4 @@ module.exports = {
     //   }
     // })
   ],
-};
+}
