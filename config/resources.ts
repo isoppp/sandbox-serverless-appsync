0d0c34983bd8bec3afe98223ca386970c541e788
isoppp <prog@d2deck.com>
2020-12-03 23:17:28 +0900

    add iam role

--- config/resources.ts
+++ config/resources.ts
@@ -1,11 +1,13 @@
 import { AWS } from '@serverless/typescript'
 import { dynamoDBResources } from './resources/dynamoDBResources'
 import { cognito } from './resources/cognito'
+import { iamRoleStatements } from './resources/iamRoleStatements'
 
 // comes from cloud formation resources https://www.serverless.com/framework/docs/providers/aws/guide/resources/#override-aws-cloudformation-resource
 export const resources: AWS['resources'] = {
   Resources: {
     ...dynamoDBResources,
     ...cognito,
+    ...iamRoleStatements,
   },
 }
